ddd5acb7b20867af420f2175458d0594b623f82c
jlloydcsapo <lloydcsapo@outlook.com>
2023-07-28 21:10:33 -0400

    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,150 +1,54 @@
-var cityPop = [
-    { 
-        city: 'Madison',
-        population: 233209
-    },
-    {
-        city: 'Milwaukee',
-        population: 594833
-    },
-    {
-        city: 'Green Bay',
-        population: 104057
-    },
-    {
-        city: 'Superior',
-        population: 27244
-    }
-];
+window.onload = setMap();
 
-var dataArray = [10,20,30,40,50];
+function setMap(){
 
-//SVG dimension variables
-var w = 900, h = 500;
+    var height = 700;
+    var width = 1250;
 
-var x = d3.scaleLinear() 
-    .range([90, 750]) 
-    .domain([0, 3]); 
+    console.log(height);
+    console.log(width);
 
-var minPop = d3.min(cityPop, function(d) {
-    return d.population;
-});
+    var map = d3.select('#map-container')
+        .append('svg')
+        .attr('class', 'map')
+        .attr('width', width)
+        .attr('height', height);
 
-var maxPop = d3.max(cityPop, function(d) {
-    return d.population;
-});
+    var projection = d3.geoAlbersUsa()
+        .scale(1200)
+        .translate([width / 2, height / 2]);
 
-var y = d3.scaleLinear()
-.range([450, 50])
-.domain([0, 700000]);
+    console.log(projection);
+        
+    var path = d3.geoPath()
+        .projection(projection);
 
-var color = d3.scaleLinear()
-.range([
-    "#FDBE85",
-    "#D94701"
-])
-.domain([
-    minPop, 
-    maxPop
-]);
+    console.log(path)
+    
+    var promises = [d3.csv("data/state_econ_data.csv"),
+                    d3.json("data/states_topo.topojson")
+                ];
 
-var yAxis = d3.axisLeft(y);
+    Promise.all(promises).then(callback);
 
- //Example 1.2 line 1...container block
- var container = d3.select("body") //get the <body> element from the DOM
-    .append("svg") //put a new svg in the body
-    .attr("width", w) //assign the width
-    .attr("height", h) //assign the height
-    .attr("class", "container") 
-    .style("background-color", "rgba(0,0,0,0.2)");
+    function callback(data) {
+        var csvData = data[0],
+            states = data[1];
+        
+        //translate topoJSON to geoJSON
+        var us_states_topo = topojson.feature(states, states.objects.us_states);
+        var us_states_features = us_states_topo.features;
+        console.log(us_states_topo);
+        console.log(us_states_features);
 
-var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
-
-var axis = container.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(50, 0)")
-    .call(yAxis);
-
-//innerRect block
-var innerRect = container.append("rect")
-    .datum(400) //put a new rect in the svg
-    .attr("width", function(d) {
-        return d * 2;
-    }) //rectangle width
-    .attr("height", function(d){
-        return d;
-    }) //rectangle height
-    .attr("class", "innerRect")
-    .attr("x", 50)
-    .attr("y", 50)
-    .style("fill", "#FFFFFF");
-
-var circles = container.selectAll(".circles")
-    .data(cityPop)
-    .enter()
-    .append("circle")
-    .attr("class", "circles") //apply a class name to all circles
-    .attr("id", function(d){
-        return d.city;
-    })
-    .attr("r", function(d){
-        //calculate the radius based on population value as circle area
-        var area = d.population * 0.01;
-        return Math.sqrt(area/Math.PI);
-    })
-    .attr("cx", function(d, i){
-        //use the index to place each circle horizontally
-        return x(i)
-    })
-    .attr("cy", function(d){
-        //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-        return y(d.population);
-    })
-    .style("fill", function(d, i){ //add a fill based on the color scale generator
-        return color(d.population);
-    })
-    .style("stroke", "#000"); //black circle stroke
-
-var labels = container.selectAll(".labels")
-    .data(cityPop)
-    .enter()
-    .append("text")
-    .attr("class", "labels")
-    .attr("text-anchor", "left")
-    .attr("x", function(d,i){
-        //horizontal position to the right of each circle
-        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-    })
-    .attr("y", function(d){
-        //vertical position centered on each circle
-        return y(d.population);
-    });
-
-     //first line of label
-var nameLine = labels.append("tspan")
-    .attr("class", "nameLine")
-    .attr("x", function(d,i){
-        //horizontal position to the right of each circle
-        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-    })
-    .text(function(d){
-        return d.city;
-    });
-
-var format = d3.format(",");
-
-    //Example 3.16 line 1...second line of label
-var popLine = labels.append("tspan")
-    .attr("class", "popLine")
-    .attr("x", function(d,i){
-       return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-     })
-    .attr("dy", "15") //vertical offset
-    .text(function(d){
-       return "Pop. " + format(d.population); //use format generator to format numbers
-    });
+        console.log(path);
+        var us_states = map.selectAll('.states')
+            .data(us_states_features)
+            .enter()
+            .append("path")
+            .attr('class', function(d) {
+                return "states " + d.properties.NAME;
+            })
+            .attr('d', path);
+    };
+}
