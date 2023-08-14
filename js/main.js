d8fc5ad52a5ab35ea7eba5b6a177c40584d4f1c4
jlloydcsapo <lloydcsapo@outlook.com>
2023-08-14 13:27:01 -0400

    Lab 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function() {
 
-var attrArray = ['GINI_INDEX','INCOME_PER_CAPITA','GDP_PER_CAPITA','POVERTY_PERCENT','COST_OF_LIVING_INDEX'];
-var expressed = attrArray[0];
+var attrArray = ['Gini Index','Unemployment Rate','Cost Of Living Index','Income Per Capita($US)','GDP Per Capita($US Millions)'];
+var expressed = attrArray[3];
 window.onload = setMap();
 
 function setMap(){
@@ -9,7 +9,7 @@ function setMap(){
     var height = window.innerHeight *.65;
     var width = window.innerWidth 
 
-    console.log(width);
+
 
     var map = d3.select('#map-container')
         .append('svg')
@@ -17,7 +17,7 @@ function setMap(){
         .attr('width', width)
         .attr('height', height);
     
-    var promises = [d3.csv("data/state_data.csv"),
+    var promises = [d3.csv("data/state_data_4.csv"),
                     d3.json("data/states_topo.topojson")
                 ];
 
@@ -49,11 +49,15 @@ function setMap(){
 
         //add coordinated visualization 
         setChart(csvData, colorScale);
+
+        //create dropdown menu
+        createDropdown(csvData);
     };   
 };
 
 //set chart
-function setChart(data, colorScale) {
+function setChart(data, colorScale) { 
+    
     var chartHeight = window.innerHeight * .25;
     var chartWidth = window.innerWidth * .97
 
@@ -61,16 +65,16 @@ function setChart(data, colorScale) {
     const marginBottom = 50;
     const marginLeft = 20;
     const radius = 9.5;
-    const padding = 1.5;
+    const padding = .5;
 
     //create linear scale 
     var x = d3.scaleLinear()
         .domain(d3.extent(data, d => d[expressed]))
         .range([marginLeft, chartWidth - marginRight]);
 
-
     const svg = d3.select("#chart-container")
         .append("svg")
+        .attr("class", 'svg-container')
         .attr("width", chartWidth)
         .attr("height", chartHeight)
         .attr("viewBox", [0, 0, chartWidth, chartHeight])
@@ -95,29 +99,44 @@ function setChart(data, colorScale) {
         .selectAll()
         .data(dodge(data, {radius: radius * 2 + padding, x: d => x(d[expressed])}))
         .join("circle")
+        .attr("class", function(d) {
+            console.log(d);
+            return d.data.NAME;
+        })
         .attr("cx", d => d.x)
         .attr("cy", d => chartHeight - marginBottom - radius - padding - d.y)
         .attr("r", radius)
         .attr("fill", function(d) {
             return colorScale(d.data[expressed]);
         })
-        .append("text")  //TRYING TO ADD TEXT HERE
+        .on("mouseover", function(event, d){
+            highlight(d.data.properties);
+        })
+        .on("mouseout", function(event, d){
+            console.log("Trying dehilight")
+            dehighlight(d.properties);
+        })
+        .on("mousemove", moveLabel)
+        .append("text") 
         .attr("x", d => d.x)
         .attr("y", d => chartHeight - marginBottom - radius - padding - d.y)
         .attr("fill", "#000")
         .attr("text-anchor", "middle")
         .text(d => d.data.ABBR)
 
-    
-    return bees.node();
-};
+    var desc = bees.append("desc")
+        .text('{"stroke": "none", "stroke-width": "0px"}');
+            
+    return bees;
+ };
 
 //dodge placement function for beeswarm - https://observablehq.com/@d3/beeswarm/2
 function dodge(data, {radius = 1, x = d => d} = {}) {
+    console.log(data)
     const radius2 = radius ** 2;
     const circles = data.map((d, i, data) => ({x: +x(d, i, data), data: d})).sort((a, b) => a.x - b.x);
     console.log(circles);
-    const epsilon = 1e-3;
+    const epsilon = 1
     let head = null, tail = null;
   
     // Returns true if circle ⟨x,y⟩ intersects with any circle in the queue.
@@ -154,7 +173,7 @@ function dodge(data, {radius = 1, x = d => d} = {}) {
       if (head === null) head = tail = b;
       else tail = tail.next = b;
     }
-  
+    console.log(circles);
     return circles;
 };
 
@@ -241,7 +260,7 @@ function joinData(us_states_features, csvData) {
 };
 
 function drawStates(us_states_features, map, path, colorScale) {
-    map.selectAll('.states')
+  var states =  map.selectAll('.states')
         .data(us_states_features)
         .enter()
         .append("path")
@@ -251,6 +270,140 @@ function drawStates(us_states_features, map, path, colorScale) {
         .attr('d', path)
         .style('fill', function(d) {
             return colorScale(d.properties[expressed]);
-        });
+        })
+        .on("mouseover", function(event, d){
+            highlight(d.properties);
+        })
+        .on("mouseout", function(event, d){
+            console.log("Trying dehilight")
+            dehighlight(d.properties);
+        })
+        .on("mousemove", moveLabel);
+
+    var desc = states.append("desc")
+        .text('{"stroke": "#d8f3dc", "stroke-width": "1px"}') 
 };  
+
+//function to create a dropdown menu for attribute selection
+function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("#map-container")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
+        });;
+
+    //add initial option
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Attribute");
+
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+};
+
+//dropdown change event handler
+function changeAttribute(attribute, csvData) {
+    //change the expressed attribute
+    expressed = attribute;
+
+    //recreate the color scale
+    var colorScale = createColorScale(csvData);
+
+    //recolor enumeration units
+    var regions = d3.selectAll(".states")
+    .transition()
+    .duration(1000)
+    .style("fill", function (d) {
+        var value = d.properties[expressed];
+        if (value) {
+            return colorScale(d.properties[expressed]);
+        } else {
+            return "#ccc";
+        }
+    });
+
+    d3.selectAll("#chart-container > *").remove();
+    setChart(csvData, colorScale);
+}
+
+//function to highlight enumeration units and bars
+function highlight(props){
+    //change stroke
+    var selected = d3.selectAll("." + props.NAME)
+        .style("stroke", "orange")
+        .style("stroke-width", "5");
+
+    setLabel(props);
+};
+
+//function to reset the element style on mouseout
+function dehighlight(props){
+    var selected = d3.selectAll("." + props.NAME)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+
+        var styleObject = JSON.parse(styleText);
+        d3.select(".infolabel")
+        .remove();
+        return styleObject[styleName];
+    };
+};
+
+//function to create dynamic label
+function setLabel(props){
+    //label content
+    var labelAttribute = "<h1>" + props[expressed] +
+        "</h1><b>" + expressed + "</b>";
+
+    //create info label div
+    var infolabel = d3.select("body")
+        .append("div")
+        .attr("class", "infolabel")
+        .attr("id", props.NAME + "_label")
+        .html(labelAttribute);
+
+    var stateName = infolabel.append("div")
+        .attr("class", "labelname")
+};
+
+function moveLabel(){
+    //get width of label
+    var labelWidth = d3.select(".infolabel")
+        .node()
+        .getBoundingClientRect()
+        .width;
+
+    //use coordinates of mousemove event to set label coordinates
+    var x1 = event.clientX + 10,
+        y1 = event.clientY - 75,
+        x2 = event.clientX - labelWidth - 10,
+        y2 = event.clientY + 25;
+
+    //horizontal label coordinate, testing for overflow
+    var x = event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1; 
+    //vertical label coordinate, testing for overflow
+    var y = event.clientY < 75 ? y2 : y1; 
+
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
+};
+     
 })();
